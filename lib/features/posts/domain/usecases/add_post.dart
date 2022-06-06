2e73719f1db9c0e0a510c5c024988a25a579afb4
basuony <mbasuony83@gmail.com>
2022-06-07 00:19:05 +0200

    Use case posts

---
+++ lib/features/posts/domain/usecases/add_post.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failures.dart';
+import '../entities/post.dart';
+import '../repositories/posts_repository.dart';
+
+class AddPostUsecase{
+  final PostsRepository repository;
+  AddPostUsecase(this.repository);
+
+  Future<Either<Failure, Unit>> call (Post post){
+    return repository.addPost(post);
+  }
+}
