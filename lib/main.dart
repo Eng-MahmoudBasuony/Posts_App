2d8e155e6b22d21423403345ecee648951697b15
basuony <mbasuony83@gmail.com>
2022-06-07 23:06:11 +0200

    pagePosts

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:clean_architecture_add_posts/core/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
 import 'features/posts/presentation/bloc/posts/posts_bloc.dart';
+import 'features/posts/presentation/pages/posts_page.dart';
 import 'injection_container.dart' as di;
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
         providers: [
-          BlocProvider(create: (_) => di.sl<PostsBloc>()),
+          BlocProvider(create: (_) => di.sl<PostsBloc>()..add(GetAllPostsEvent())),
           BlocProvider(create: (_) => di.sl<AddDeleteUpdatePostBloc>()),
         ],
         child: MaterialApp(
