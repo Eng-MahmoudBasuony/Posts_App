b5589b2a0d9ce1d4399a3533206acb1b34b0d255
basuony <mbasuony83@gmail.com>
2022-06-07 22:07:39 +0200

    MultiBlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,28 @@
 import 'package:clean_architecture_add_posts/core/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
+import 'features/posts/presentation/bloc/posts/posts_bloc.dart';
+import 'injection_container.dart' as di;
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Posts App',
-      theme: appTheme,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("posts"),
-        ),
-        body: Center(
-          child: Text('posts'),
-        ),
-      ),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => di.sl<PostsBloc>()),
+          BlocProvider(create: (_) => di.sl<AddDeleteUpdatePostBloc>()),
+        ],
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: appTheme,
+            title: 'Posts App',
+            home: PostsPage()));
   }
 }
